7f98ebbb2e9fc1b532a8f69fe5b95429b45e501f
sparrowsp <62995900+sparrowsp@users.noreply.github.com>
2020-05-23 18:15:34 +0300

    404 not found

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@38r8_lyhed^lawx48b+#32^_j&rv+-xj^a9^y^$bzf(9b9$nf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
